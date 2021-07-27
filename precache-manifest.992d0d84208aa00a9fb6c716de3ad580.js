7a02ac2b2523c42f009a3e9742822a0923ff3a94
Mikk78 <teshcenko93@gmail.com>
2021-07-27 20:09:01 +0200

    Updates

---
+++ precache-manifest.992d0d84208aa00a9fb6c716de3ad580.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88318a91e35f441de9054500cda00036",
+    "url": "/react-web-site/index.html"
+  },
+  {
+    "revision": "aa466303d64b69aea7cd",
+    "url": "/react-web-site/static/css/main.374493b4.chunk.css"
+  },
+  {
+    "revision": "ea89174ceb58f81c5859",
+    "url": "/react-web-site/static/js/2.4b73c6cb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-web-site/static/js/2.4b73c6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa466303d64b69aea7cd",
+    "url": "/react-web-site/static/js/main.46646fc2.chunk.js"
+  },
+  {
+    "revision": "497c117b8973b8ba9b86",
+    "url": "/react-web-site/static/js/runtime-main.8d3e746e.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/react-web-site/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/react-web-site/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/react-web-site/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/react-web-site/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "c64ec5c8d3ea8c3743ca51c1fa59e50e",
+    "url": "/react-web-site/static/media/img-5.c64ec5c8.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/react-web-site/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/react-web-site/static/media/img-7.66001cf8.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/react-web-site/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "523a89f64e53deb7aa02e9ff0fa6d18f",
+    "url": "/react-web-site/static/media/img-9.523a89f6.jpg"
+  },
+  {
+    "revision": "229f5279475d67e3cf902f7ab5cd818b",
+    "url": "/react-web-site/static/media/video-1.229f5279.mp4"
+  }
+]);
